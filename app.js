cfb19782e5c9139aec1109b9f92fc88c2be09116
SimonImanigirimpuhwe <softimag08@gmail.com>
2022-02-18 20:25:19 +0200

    ft(morgan): add morgan

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("./models/db")
 const userRoutes = require("./routes/user")
+const morgan = require("morgan")
 
 const app = express();
 
 app.use(express.json())
+app.use(morgan("dev"))
 
 //routes middleware
 app.use("/users", userRoutes)
