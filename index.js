55c14a1f88c17ca7de060633fa886a333a9bf22a
SimonImanigirimpuhwe <softimag08@gmail.com>
2022-02-18 20:14:15 +0200

    ft(git2): test rebase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const  app = require("./app");
 const PORT = process.env.PORT || 3000
 
 // eslint-disable-next-line
-const server = app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports =  server
 
